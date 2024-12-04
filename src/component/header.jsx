52c89a693b316993df1cdc08cfd4b8ee56a214a2
ㄏㄩ <hyu1@hyu1deMacBook-Pro.local>
2024-12-04 17:00:54 +0700

    forth

--- src/component/header.jsx
+++ src/component/header.jsx
@@ -11,7 +11,7 @@ export default function Header() {
         
         <div className='navigationbar'>
         
-          <a href="https://linktr.ee/orientalcomputer" className="nav-link"><h1>Contact</h1></a>
+          <a href="https://www.linktr.ee/orientalcomputer" className="nav-link"><h1>Contact</h1></a>
           <a href="https://www.google.com/maps/dir/-7.0032046,110.4857935/oriental+computer/@-7.0056495,110.4190403,13z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x2e708d1a73b8d3bb:0x4313b72c95fddae7!2m2!1d110.4327799!2d-7.0063473?entry=ttu&g_ep=EgoyMDI0MTEyNC4xIKXMDSoASAFQAw%3D%3D" className="nav-link"><h1>Location</h1></a>
         </div>
       </div>
